3c6217e487fa1040d203ffc51f835912e47a75d0
srbony <srabonycse@gmail.com>
2022-10-10 22:43:57 +0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Blog/Blog';
+import Statistics from './Components/Statistics/Statistics';
+import Topics from './Components/Topics/Topics';
 import Main from './Layout/Main';
 
 function App() {
 
   const router = createBrowserRouter([
 
-    { path: '/', element: <Main></Main> }
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+
+        { path: '/', element: <Topics></Topics> },
+        { path: '/topics', element: <Topics></Topics> },
+        { path: '/statistics', element: <Statistics></Statistics> },
+        { path: '/blog', element: <Blog></Blog> }
+      ]
+    }
 
 
   ])
---
+++ src/Components/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>THIS IS STATISTICS</h2>
+        </div>
+    );
+};
+
+export default Statistics;
